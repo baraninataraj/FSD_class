622b227746e11f4e2b0f41f7e23b1bb5db6c0025
baraninataraj <baraninataraj52@gmail.com>
2024-07-06 00:23:56 +0530

    changes in day6 task file

--- Day-06_Task/script.js
+++ Day-06_Task/script.js
@@ -4,8 +4,8 @@ var todosAPi = "https://restcountries.com/v3.1/all";
 var todoRequest = new XMLHttpRequest();
 
 todoRequest.onreadystatechange = function () {
-  if (this.readyState === XMLHttpRequest.DONE && this.status === 200){
   var countryData = JSON.parse(this.response);
+  console.log(countryData);
   let asia=countryData.filter((element)=>{
     if(element.region==='Asia'){
         return element.name;
@@ -13,7 +13,6 @@ todoRequest.onreadystatechange = function () {
 });
 console.log(asia);
 };
-}
 
 todoRequest.open("GET", todosAPi);
 todoRequest.send();
@@ -28,13 +27,12 @@ var todosAPi = "https://restcountries.com/v3.1/all";
 var todoRequest = new XMLHttpRequest();
 
 todoRequest.onreadystatechange = function () {
-  if (this.readyState === XMLHttpRequest.DONE && this.status === 200){
   var countryData = JSON.parse(this.response);
+  console.log(countryData);
   let pop = countryData.filter((element) => {
     return element.population < 200000;
 })
 console.log(pop);}
-}
 
 todoRequest.open("GET", todosAPi);
 todoRequest.send();
@@ -42,18 +40,46 @@ todoRequest.send();
 console.log(todoRequest);
 
 //Print the following details name, capital, flag, using forEach method
-var request=new XMLHttpRequest();
-request.open('GET','https://restcountries.com/v3.1/all','true');
-request.send();
-request.onload=function (){
-    var countryData=JSON.parse(this.response);
-      countryData.forEach((element) => {
-        console.log(element.name);
-        console.log(element.capital);
-        console.log(element.flags);
-      })}
+var todosAPi = "https://restcountries.com/v3.1/all";
+
+var todoRequest = new XMLHttpRequest();
+
+todoRequest.onreadystatechange = function () {
+  
+  var countryData = JSON.parse(this.response);
+  console.log(countryData);
+  countryData.forEach((element) => {
+    console.log(element.name, element.capital, element.flags);
+  })}
+
+
+todoRequest.open("GET", todosAPi);
+todoRequest.send();
+
+console.log(todoRequest);
 
 //Print the total population of countries using reduce method
+//var todosAPi = "https://restcountries.com/v3.1/all";
 
+//var todoRequest = new XMLHttpRequest();
+
+//todoRequest.onreadystatechange = function () {
+ // var countryData = JSON.parse(this.response);
+ // console.log(countryData);
+  //const poulation = function pop((carry, nextElement){
+   // console.log(carry, nextElement);
+    //if (typeOf carry === "object"){
+    //    return carry.population + nextElement.population
+    //} else { return carry + nextElement.population
+    //        }
+    //var output = countryData.reduce(pop);
+    //console.log(output);
+   // });
+//};
+
+//todoRequest.open("GET", todosAPi);
+//todoRequest.send();
+
+console.log(todoRequest);
 
 //Print the country that uses US dollars as currency.
